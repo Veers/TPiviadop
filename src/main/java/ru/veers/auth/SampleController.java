d07b45fa2041b0e28c4aca72abbf384eea848442
Yuryev-KV <KVYuryev@sberbank.ru>
2020-10-09 16:47:19 +0300

    change again

---
+++ src/main/java/ru/veers/auth/SampleController.java
@@ -0,0 +1,13 @@
+package ru.veers.auth;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SampleController {
+    @GetMapping("/sample")
+    public String showForm() {
+        return "sample";
+    }
+
+}
